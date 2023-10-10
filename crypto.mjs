ffdc527cd9fac5c8bbd4384b5b149fd70e05a0fd
Daniel Sportes <daniel@sportes.fr>
2023-10-10 14:56:30 +0200

    Reprise Id Tk

--- crypto.mjs
+++ crypto.mjs
@@ -10,32 +10,44 @@ function rnd6 () {
   return r
 }
 
-function iToL6 (n) {
-  let x = ''
-  for (let i = 0, j = n; i < 6; i++) { x = String.fromCharCode(65 + (j % 26)) + x; j = Math.floor(j / 26) }
+/* retourne un code à 6 lettres majuscules depuis l'id d'un ticket */
+export function idTkToL6 (t) {
+  const am = Math.floor(t / d10)
+  const m = am % 100
+  const a = Math.floor(am / 100)
+  let x = String.fromCharCode(a % 2 === 0 ? 64 + m : 76 + m)
+  for (let i = 0, j = (t % d10); i < 5; i++) { x += String.fromCharCode(65 + (j % 26)); j = Math.floor(j / 26) }
   return x
 }
 
-function l6ToI (s) {
-  let n = 0
-  for (let i = 0; i < 6; i++) { n = (n * 26) + (i < s.length ? (s.charCodeAt(i) - 65) : 65) }
-  return n
+/* Génère l'id d'un ticket: aa mm rrr rrr rrr r */
+const d10 = 10000000000
+export function genIdTk (a, m) {
+  const x1 = (((a % 100) * 100) + m)
+  return (rnd6() % d10) + (x1 * d10)
 }
 
-function genTk (a, m) {
-  const l = iToL6(rnd6())
-  const c = String.fromCharCode(a % 2 === 0 ? 64 + m : 76 + m)
-  return c + l.substring(1)
+/* Retourne l'année et le mois depuis un code à 6 lettres */
+export function amDeL6 (l6) {
+  const a = new Date().getFullYear()
+  const pa = a % 2
+  const c = l6.charCodeAt(0) - 64
+  const [p, m] = c > 12 ? [1, c - 12] : [0, c]
+  return [p === pa ? a : a - 1, m]
 }
 
-function mDeTk (t) {
-  const c = t.charCodeAt(0) - 65
-  return c > 12 ? [1, c - 11] : [0, c + 1]
+{
+  const t = genIdTk(2022, 1)
+  const l6 = idTkToL6(t)
+  console.log(t, l6, amDeL6(l6))
 }
-
-const t = genTk(2020, 1)
-const b = l6ToI(t)
-const c = iToL6(b)
-const d = l6ToI(c)
-const [a, m] = mDeTk(t)
-console.log(t, b, c, d, a, m)
+{
+  const t = genIdTk(2023, 1)
+  const l6 = idTkToL6(t)
+  console.log(t, l6, amDeL6(l6))
+}
+{
+  const t = genIdTk(2022, 12)
+  const l6 = idTkToL6(t)
+  console.log(t, l6, amDeL6(l6))
+}
