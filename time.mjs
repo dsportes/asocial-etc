cd029ace4ade067430473c676bd4df1a5818e68d
Daniel Sportes <daniel@sportes.fr>
2025-09-02 15:53:18 +0200

    time

--- time.mjs
+++ time.mjs
@@ -1,11 +1,14 @@
-const t0 = Date.UTC(2000, 0, 1, 0, 0, 0)
-console.log('2000', t0)
-// let x = Date.now()
-const x = Date.UTC(2099, 11, 31, 23, 59, 59, 999)
-const ms = x % 86400000
-const nj = Math.floor(x / 86400000)
-console.log('2099', x, nj, ms)
-const y = (nj * 100000000) + ms
-console.log(y, (''+y).length)
+let x = Date.UTC(2025, 0, 1, 0, 0, 0, 0)
+let ms = x % 86400000
+let nj = Math.floor(x / 86400000)
+let y = (nj * 100000000) + ms
+console.log('2025', y, nj, ms, (''+y).length)
+
+x = Date.UTC(2099, 11, 31, 23, 59, 59, 999)
+ms = x % 86400000
+nj = Math.floor(x / 86400000)
+y = (nj * 100000000) + ms
+console.log('2099', y, nj, ms, (''+y).length)
+
 const n = Number.MAX_SAFE_INTEGER
 console.log (n, (''+n).length)
